a6244d1414e31e73bdbb5241edbac8013cd1637a
sangsicp_lgtm <sangsicp@gmail.com>
2026-04-02 20:04:35 +0530

    New Assignment

---
+++ playwright-training/tests/Locators.ts/assignments_2.spec.ts
@@ -0,0 +1,16 @@
+import {test,expect} from '@playwright/test'
+
+test("Creatio CRM XPath Locators",async ({page})=>{
+
+
+    await page.goto("https://accounts.creatio.com/login/alm");
+
+    //img[@alt='logo']
+
+
+
+
+
+
+
+});
